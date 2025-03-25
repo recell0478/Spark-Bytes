580f8d9b0e106b4924d92e93d22d0e12e26c7a11
Khang Le <lhankhang2012@gmail.com>
2025-03-25 19:29:07 -0400

    Sign In page

---
+++ my-app/src/pages/SignIn.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import type { NextPage } from 'next';
+import { Layout } from 'antd';
+
+const SignIn: NextPage = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log('Sign In:', {email, password})
+    };
+
+    return (
+        <Layout>
+            <div className='container'>
+                <h2>Welcome to Boston Dynamics SparkBytes!</h2>
+
+                <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column'}}>
+                    <label htmlFor="email" style= {{ textAlign: 'left'}}>BU Email</label>
+                    <input 
+                        id="email"
+                        type="email"
+                        placeholder="Enter your BU email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required style={{ marginBottom: '1rem', padding: '0.5rem'}}
+                    />
+
+                    <label htmlFor="password" style={{ textAlign: 'left'}}>Password</label>
+                    <input 
+                        id="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required style={{ marginBottom: '1rem', padding: '0.5rem'}}
+                    />
+
+                    <button
+                        type="submit"
+                        style={{
+                            backgroundColor: '#c00',
+                            color: '#fff',
+                            padding: 'o.75rem',
+                            border: 'none',
+                            cursor: 'pointer',
+                            marginBottom: '1rem',
+                        }}
+                    >
+                        Login
+                    </button>
+                </form>
+
+                <a href="#" style={{ display: 'block', margin: '1rem 0'}}>Forgot Password</a>
+
+                <a href="/signup">
+                    <button
+                        style={{
+                            backgroundColor: '#c00',
+                            color: '#fff',
+                            padding: '0.75rem',
+                            border: 'none',
+                            cursor: 'pointer',
+                        }}
+                        >
+                        Sign Up
+                    </button>
+                </a>
+
+                <footer>©BU Spark Bytes 2025</footer>
+            </div>
+        </Layout>
+    );
+};
+
+export default SignIn;
