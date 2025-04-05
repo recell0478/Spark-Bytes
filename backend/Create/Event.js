d9f0267110ef37229a3cb2e01c8309eb7c4115f9
clbrown02 <chrislbr@bu.edu>
2025-04-05 16:21:54 -0400

    included create folder and event creation

---
+++ backend/Create/Event.js
@@ -0,0 +1,22 @@
+import { supabase } from '../backend/supabaseClient'
+
+export async function createEvent({ name, spots_remaining, description, time, allergens }) {
+  const { data, error } = await supabase
+    .from('Events')
+    .insert([
+      {
+        name,
+        spots_remaining,
+        description,
+        time,
+        allergens
+      }
+    ])
+
+  if (error) {
+    console.error('Error creating event:', error.message)
+    return { success: false, error }
+  }
+
+  return { success: true, event: data[0] }
+}
